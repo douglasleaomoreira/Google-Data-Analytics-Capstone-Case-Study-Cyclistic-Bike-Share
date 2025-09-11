c2f561af036a811ddf388fa085f0f858356dc087
Douglas Leao Moreira <douglasleaoamm@gmail.com>
2025-09-11 18:59:26 +1000

    Add files via upload

---
+++ 1. Data Preparation.R
@@ -0,0 +1,57 @@
+library(tidyverse)  #helps wrangle data
+# Use the conflicted package to manage conflicts
+library(conflicted)
+
+# Set dplyr::filter and dplyr::lag as the default choices
+conflict_prefer("filter", "dplyr")
+conflict_prefer("lag", "dplyr")
+
+#=====================
+# STEP 1: COLLECT DATA
+#=====================
+# # Upload Divvy datasets (csv files) here
+q1_2019 <- read_csv("Divvy_Trips_2019_Q1.csv")
+q1_2020 <- read_csv("Divvy_Trips_2020_Q1.csv")
+
+
+#====================================================
+# STEP 2: WRANGLE DATA AND COMBINE INTO A SINGLE FILE
+#====================================================
+# Compare column names each of the files
+# While the names don't have to be in the same order, they DO need to match 
+# perfectly before we can use a command to join them into one file
+colnames(q1_2019)
+colnames(q1_2020)
+
+# Rename columns  to make them consistent with q1_2020 (as this will be the 
+# supposed going-forward table design for Divvy)
+
+(q1_2019 <- rename(q1_2019
+                   ,ride_id = trip_id
+                   ,rideable_type = bikeid
+                   ,started_at = start_time
+                   ,ended_at = end_time
+                   ,start_station_name = from_station_name
+                   ,start_station_id = from_station_id
+                   ,end_station_name = to_station_name
+                   ,end_station_id = to_station_id
+                   ,member_casual = usertype
+))
+
+# Inspect the dataframes and look for incongruencies
+str(q1_2019)
+str(q1_2020)
+
+# Convert ride_id and rideable_type to character so that they can stack 
+# correctly
+q1_2019 <-  mutate(q1_2019, ride_id = as.character(ride_id)
+                   ,rideable_type = as.character(rideable_type)) 
+
+# Stack individual quarter's data frames into one big data frame
+all_trips <- bind_rows(q1_2019, q1_2020)#, q3_2019)#, q4_2019, q1_2020)
+
+# Remove lat, long, birthyear, and gender fields as this data was dropped 
+# beginning in 2020
+all_trips <- all_trips %>%  
+  select(-c(start_lat, start_lng, end_lat, end_lng, birthyear, gender,  
+            "tripduration"))
